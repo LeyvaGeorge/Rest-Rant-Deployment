2ed650f39abb5ba5c5b0c1e56e1a6b0b4a8fd04f
George Leyva <leyvageorge@hotmail.com>
2023-07-25 19:19:45 -0700

    added front end

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app'; //Might need to change to App not app
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
